da796b22cb039dca095db61c2123569324087da1
zerow.kim <0wkim@naver.com>
2025-11-01 00:22:58 +0900

    feat: 출력 메시지 상수화

---
+++ src/constants/Messages.js
@@ -0,0 +1,7 @@
+export const MATCH_MESSAGES = Object.freeze({
+    THREE_MATCH: "3개 일치 (5,000원)",
+    FOUR_MATCH: "4개 일치 (50,000원)",
+    FIVE_MATCH: "5개 일치 (1,500,000원)",
+    FIVE_BONUS_MATCH: "5개 일치, 보너스 볼 일치 (30,000,000원)",
+    SIX_MATCH: "6개 일치 (2,000,000,000원)"
+});
